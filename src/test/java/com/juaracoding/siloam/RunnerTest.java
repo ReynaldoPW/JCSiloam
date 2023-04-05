fc5f9a7c82b8fb90827456e6e4341dc31671e5e4
ranggaaryo79 <ranggaaryo79@gmail.com>
2023-04-05 15:16:08 +0700

    commit test

--- src/test/java/com/juaracoding/siloam/RunnerTest.java
+++ src/test/java/com/juaracoding/siloam/RunnerTest.java
@@ -7,8 +7,10 @@ import io.cucumber.testng.CucumberOptions;
         features = {"src/main/resources/features/01LoginAdmin.feature",
                 "src/main/resources/features/02LoginSales.feature",
                 "src/main/resources/features/03InputDataSales.feature",
-                "src/main/resources/features/04TTDDigitalSales.feature",
-                "src/main/resources/features/05FormReturn.feature"},
+                "src/main/resources/features/04UploadSales.feature",
+                "src/main/resources/features/05TTDDigitalSales.feature",
+                "src/main/resources/features/06FormReturn.feature",
+                "src/main/resources/features/07ViewExport.feature"},
         glue = {"com.juaracoding.siloam"},
         plugin = {"pretty","html:target/cucumber-report.html","json:target/cucumber.json"}
 )
